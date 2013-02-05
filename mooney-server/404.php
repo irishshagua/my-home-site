b90dbab5703a496864f7a1175c65ce4db0fc6ee1
Brian Mooney <brian_satchwannabe@hotmail.com>
2013-02-05 23:20:42 +0000

    New 404 err page

---
+++ mooney-server/404.php
@@ -0,0 +1,48 @@
+<?php 
+    $requested_url = $_SERVER['REQUEST_URI']; 
+    
+    $logo = "     __  __       ____     ____        __    __    ____    __    __\\n    /  \\\/  \\\     / __ \\\   / __ \\\      /  \\\  /  /  |  __|   \\\ \\\  / /\\n   /  /\\\/\\\  \\\   | |  | | | |  | |    /    \\\/  /   | |__     \\\ \\\/ /\\n  /  /    \\\  \\\  \\\ |__| / \\\ |__| /   /  /\\\    /    | |__      |  |\\n /__/      \\\__\\\  \\\____/   \\\____/   /__/  \\\__/     |____|     |__|\\n    _______   _______           __       __               _______\\n   /  ____/  |  _____|  ______  \\\ \\\     / /  ______      /  __   \\\ \\n  /  /_      |  |__    |  __  \\\  \\\ \\\   / /  |  ____|    /  |__|  | \\n  \\\__  \\\     |   __|   | |__| |   \\\ \\\_/ /   |  |__     /  __   _/ \\n     \\\  \\\    |  |____  |  _  /     \\\___/    |   __|   /  /  \\\  \\\ \\n   __/  /    |_______| | | \\\ \\\              |  |___  /__/    \\\__\\\ \\n  /____/               |_|  \\\_\\\             |______|";
+    
+    $term_header = "\"$logo \\n\\nMooney {: Server (http://www.mooney-server.tk)"
+                    . "\\nA 404 code was returned when trying to access the following URI: $requested_url"
+                    . "\\nEither you have requested a link which doesn't exist, or Zombie Aliens have already eaten $requested_url"
+                    . "\\nUse the `help` command to see your available options\\n\\n\"";
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/jquery.terminal.css"></script>
+        
+        <script src="js/jquery.js"></script>
+        <script src="js/jquery.terminal.js"></script>
+    </head>
+    <body id="term" class="terminal">
+        <script>
+            $('#term').terminal(function(command, term) {
+                    if (command == 'help') {
+                        term.echo("Below are a list of the supported commands:\n\n\t + clear: Clear the Screen\n\t + home: Return To Main Page\n\t + other: Suggest some good alternate sites to read\n\t + scan: perform self diagnostics");
+                        
+                    } else if (command == 'home') {
+                        window.location.href = 'http://www.mooney-server.tk';
+                        term.echo("Returning to Home Page...");
+                        term.set_prompt("");
+                    } else if (command == "scan") {
+                        term.error("Diagnostics Have Failed!");
+                    } else if (command == "other") {
+                        term.echo("call php to get some random links");
+                    } else {
+                        term.error('Illegal Command [' + command + ']:\n\tUse `help` command to list your options');
+                    }
+                }, 
+                { 
+                    prompt: '$: ', 
+                    greetings: <? echo $term_header; ?>,
+                    name: 'MooneyServer 404',
+                    onClear: function(t) {
+                        t.echo("<? echo $logo; ?>\n\n");
+                    }
+                });
+        </script>
+    </body>
+</html>
