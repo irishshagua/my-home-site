c791404ff6253d7b9bda9f3a2fc0f86e93538cf4
Brian Mooney <brian_satchwannabe@hotmail.com>
2013-02-20 22:30:18 +0000

    add accounts CI info dialog

--- mooney-server/freedom_travel_android_app.php
+++ mooney-server/freedom_travel_android_app.php
@@ -10,7 +10,23 @@
     
       <div id="site_content">
         <!-- Page Specific Content Goes Here -->
-        <p>Some test content</p>
+        <h3>Description</h3>
+        <p><a href="http://www.freedomtravel.ie">Freedom Travel</a> is a travel agency that my sister runs. I promised her that I'd try and do her a phone app for
+        her birthday present (cheap... I know!) and this is the end result. It's a <i>Deal of the Week</i> app. It is built mostly with HTML5 technologies so should
+        in theory run on any smart phone, but I've only tested it so far on Android. Plus the launch code is Android specific so there would be some extra work
+        required to actually et this working on other platforms but if I get the time, I'll look into getting it running on Firefox OS.</p>
+        
+        <h3>Technology</h3>
+        <p>The UI is designed using HTML5 (currently uses JQuery Mobile UI for the widgets but I'm not happy with the transitions so this may change). The data store
+        s a cloud based Mongo DB instance, <a href="https://mongolab.com/welcome/">Mongo Labs</a> are hosting that. There is more info on the tech side on the project
+        <a href="https://github.com/irishshagua/freedom-travel-deals-app/wiki/Freedom-Travel-Deals-App">wiki</a> on github.</p>
+        
+        <h3>Downloads</h3>
+        <h4>Deals Content Management System</h4>
+        <a href="http://ubuntuone.com/6LVjHPEGNOFDTigXFNbBwp">Download CMS</a>
+        
+        <h4>Freedom Travel Deals Android App</h4>
+        <a href="http://ubuntuone.com/4u4ZF9NnI86TiaVFkbWm0Y">Download App</a>
       </div>
     
       <? echo WebSiteTemplate::get_footer_bar(); ?>
--- mooney-server/proj_accounts.php
+++ mooney-server/proj_accounts.php
@@ -1,16 +1,61 @@
-<?php include 'php-cms/templating/WebSiteTemplate.php' ?>
+<?php 
+  include 'php-cms/templating/WebSiteTemplate.php';
+  
+  # Check the current build status
+  $process = curl_init("https://gradletest-mooneyserver.rhcloud.com/job/Gradle_Accounts_build/lastBuild/api/json");
+  curl_setopt($process, CURLOPT_USERPWD, "observer:83b6ec208dba4c8571410c7aed42c95e");
+  curl_setopt($process, CURLOPT_TIMEOUT, 20);
+  curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
+  
+  $json_response = json_decode(curl_exec($process));
+  
+  if ($json_response != null) {
+    $build_url = $json_response->url;
+    $last_built = $json_response->id;
+    $build_result = $json_response->result;
+  } else {
+    $build_url = "";
+    $last_built = "";
+    $build_result = "ERROR";
+  }
+  
+  if ($build_result === "SUCCESS") {
+    $build_result = '<span style="color: #00ff00">SUCCESS</span>';
+  } else {
+    $build_result = '<span style="color: #ff0000">' . $build_result . '</span>';
+  }
+?>
 
 <!DOCTYPE HTML>
 <html>
-  <? echo WebSiteTemplate::get_basic_head(); ?>
+  <? 
+    $add_js_libs = array("http://code.jquery.com/jquery-1.9.1.js", "http://code.jquery.com/ui/1.10.1/jquery-ui.js");
+    $add_css_libs = array("http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css");
+    echo WebSiteTemplate::get_basic_head($add_js_libs, $add_css_libs); 
+  ?>
   <body>
+    <div id="ci-dialog" title="Accounts Build Status">
+        <h4><a href="https://gradletest-mooneyserver.rhcloud.com/" target="_blank">Jenkins</a> Build Status</h4>
+        <p><a href="<? echo $build_url ?>" target="_blank">Accounts Build</a>:
+          <ul>
+            <li>Status: <? echo $build_result ?></li>
+            <li>Last Built: <? echo $last_built ?></li>
+          </ul>
+        </p>
+    </div>
     <div id="main">
     
       <? echo WebSiteTemplate::get_nav_bar('proj_accounts'); ?>
     
       <div id="site_content">
         <!-- Page Specific Content Goes Here -->
-        <p>This is a project to create a home accounts system. The app can be viewed <a href="/accounts">here</a> and is hosted on <a href="https://openshift.redhat.com">OpenShift</a>. The build system is currently controlled by <a href="https://gradletest-mooneyserver.rhcloud.com">Jenkins</a>. I'll update all this crap properly later...</p>
+        <p>This is a project to create a home accounts system. The app can be viewed <a href="/accounts">here</a> and is hosted on <a href="https://openshift.redhat.com">OpenShift</a>. The build system is currently controlled by <a href="https://gradletest-mooneyserver.rhcloud.com" target="_blank">Jenkins</a>. I'll update all this crap properly later...</p>
+        
+        <h3>Build Status</h3>
+        <p id="check-build-btn" style="float: right; cursor: hand; cursor: pointer;">
+          <img src="images/jenkins_link.png"/><br/>
+          C.I. Build Status
+        </p>
       </div>
     
       <? echo WebSiteTemplate::get_footer_bar(); ?>
@@ -18,5 +63,30 @@
     </div>
   
     <? echo WebSiteTemplate::get_tail_loaded_js(); ?>
+    <script type="text/javascript">
+      $(function() {
+        $("#ci-dialog").dialog({
+          autoOpen: false,
+          modal: true,
+          show: {
+            effect: "highlight",
+            duration: 2000
+          },
+          hide: {
+            effect: "puff",
+            duration: 700
+          },
+          buttons: {
+            Ok: function() {
+              $(this).dialog("close");
+            }
+          }
+        });
+        
+        $("#check-build-btn").click(function() {
+          $("#ci-dialog").dialog("open");
+        });
+      });
+    </script>
   </body>
 </html>
