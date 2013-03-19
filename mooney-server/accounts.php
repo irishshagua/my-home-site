28b58f0e365028c7c62aed735f9777e82fb3e85a
Brian Mooney <brian_satchwannabe@hotmail.com>
2013-03-19 22:44:27 +0000

    Add Toggle Sitch for Accounts Page

--- mooney-server/accounts.php
+++ mooney-server/accounts.php
@@ -7,9 +7,9 @@
   <div id="main">
     
     <? echo WebSiteTemplate::get_nav_bar('accounts'); ?>
-    <ul class='toggle'>
-    	<li class='selected'><button>Live</button></li>
-    	<li><button>Dev</button></li>
+    <ul class='toggle floatright'>
+      <li class='selected'><a>Live</a></li>
+      <li><a>Dev</a></li>
     </ul>
     <iframe id='accounts-instance' src="https://devaccounts-devmooneyserver.rhcloud.com/AccountsUI" height="800" width="100%"></iframe>
     
@@ -18,6 +18,28 @@
   </div>
   
   <? echo WebSiteTemplate::get_tail_loaded_js(); ?>
-  <script 
+  <script>
+    $(document).ready(function() {
+      accounts_instances = {
+        'Live':'https://devaccounts-devmooneyserver.rhcloud.com/AccountsUI', 
+        'Dev':'https://accounts-mooneyserver.rhcloud.com/AccountsUI'
+      };
+
+      $('ul.toggle li').click(function(event) {
+        if ($(this).attr('class') == 'selected') return;
+        
+        // Remove selected css from all li's
+        $(this).parent().find('li').each(function() {
+            $(this).removeClass('selected');
+        });
+        
+        // Add selected to clicked li
+        $(this).addClass('selected');
+        
+        // Fire iFrame Reload
+        $('#accounts-instance').attr('src', accounts_instances[$(this).text()]);
+      });
+    });
+  </script>
 </body>
 </html>
