f158ecd31fd5998d8deb98c31deb4de6d41ca6fc
Brian Mooney <brian_satchwannabe@hotmail.com>
2013-04-11 22:17:11 +0100

    Add irish pub metrics

--- mooney-server/pub_list.php
+++ mooney-server/pub_list.php
@@ -16,6 +16,13 @@
         <p>This is a simple map to keep track of the pubs that me and my brother have drank in, in Dublin. 
     	It is our stupid belief that we will one day get them all.</p>
         <p>If you have trouble viewing the map, try this <a href="proj/pubs/map.html">link</a></p>
+        <p>A full listing of licenced premises in Dublin can be viewed on the <a href="http://www.revenue.ie/en/tax/excise/index.html" target="_blank">revenue</a> site. Download the Liquor Licences spreadsheet. Couple of interesting facts: 
+            <ul>
+                <li>Dublin has 667 licenced pubs of the national total of 6513 (10.24%)</li>
+                <li>However, it has 19.4% of the total licences (inclusive of Hotel, Brewer, Boat, Plane, etc...)</li>
+            </ul>
+            That would kind of suggest that there is more even distribution of pubs in the country than hotels and restaurants....
+        </p>
     </div>
     
     <? echo WebSiteTemplate::get_footer_bar(); ?>
